2f86f6070149e15a065d1f4df599640ec826cace
Odinei Ribeiro Da Rocha Júnior <odinei.ribeiro@live.com>
2016-02-13 14:22:59 -0200

    Devise configured

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210030645) do
+ActiveRecord::Schema.define(version: 20160213155214) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string   "name"
@@ -68,5 +68,23 @@ ActiveRecord::Schema.define(version: 20160210030645) do
   add_index "recipes", ["description_id"], name: "index_recipes_on_description_id"
   add_index "recipes", ["difficulty_id"], name: "index_recipes_on_difficulty_id"
   add_index "recipes", ["preference_id"], name: "index_recipes_on_preference_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
