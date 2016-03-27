17830fa45706ea5df0d10edb8649ba47d3b5b920
Odinei Ribeiro Da Rocha Júnior <odinei.ribeiro@live.com>
2016-03-27 01:19:26 -0300

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+# This is only a test
 
 ActiveRecord::Schema.define(version: 20160214184452) do
 
